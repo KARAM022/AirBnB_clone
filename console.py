14fb740c72a7042320474c92030a3fdc66f56c2f
KARAM022 <my0fav0library@gmail.com>
2024-03-05 01:51:39 +0100

    6. Console 0.0.1 - done - 0x02

--- console.py
+++ console.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
-"""Module for HBNBCommand class."""
+"""HBNBCommand class"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for HBNB project."""
+    """The entry point of the command interpreter"""
 
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit the program."""
+        """exit"""
         return True
 
     def help_quit(self):
-        """Print help message for quit command."""
+        """help for exit"""
         print("Quit command to exit the program \n")
 
     def do_EOF(self, line):
-        """Handle EOF (Ctrl+D) to exit the program."""
+        """quit whith EOF"""
         print("")
         return True
 
     def emptyline(self):
-        """Do nothing on empty line."""
+        """noyhing"""
         pass
 
 
